0f0c977267a9f4a390a61445865377f29646157e
sunkarasai240@gmail.com <sunkarasai240@gmail.com>
2022-11-13 11:32:58 +0530

    java code

---
+++ src/main/java/com/Demo/demo/JavaProjectApplication.java
@@ -0,0 +1,14 @@
+package com.Demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectApplication.class, args);
+		System.out.println("this is my first java application!!");
+	}
+
+}
